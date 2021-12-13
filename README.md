8732993812f70320aab13054b05943b57edd92ca
taotaoli-lt01 <ltt124356871@126.com>
2021-12-13 11:11:44 +0800

    update first version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # bayer2rgb
 bayer2rgb with python
+
+
+python .\raw2rgb.py -h
+usage: raw2rgb.py [-h] [--rawfile RAWFILE] [--width WIDTH] [--height HEIGHT]
+                  [--pattern PATTERN] [--depth DEPTH]
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --rawfile RAWFILE  input raw file name
+  --width WIDTH      raw image width
+  --height HEIGHT    raw image height
+  --pattern PATTERN  raw image bayer pattern: [1:RGGB, 2:GRBG, 3:GBRG, 4:BGGR]
+  --depth DEPTH      raw image depth [8, 10, 12, 14, 16]
+  
+
+eg.
+python .\raw2rgb.py --rawfile raw8_1600x1200_RGGB.raw --width 1600 --height 1200 --pattern 1 --depth 8
---
+++ raw2rgb.py
@@ -0,0 +1,84 @@
+import numpy as np
+import cv2
+import argparse
+
+
+class bayerFormat:
+    BayerRGGB = 1
+    BayerGRBG = 2
+    BayerGBRG = 3
+    BayerBGGR = 4
+
+
+def showColorImg(imgName, img):
+    cv2.namedWindow(imgName, cv2.WINDOW_NORMAL)
+    cv2.imshow(imgName, img)
+
+
+def closeWindows():
+    cv2.waitKey(0)
+    # 释放窗口
+    cv2.destroyAllWindows()
+
+
+def convertBayer2RGB(bayerFile, imgWidth, imgHeight, bitDeepth, bayerPattern):
+    if bitDeepth == 8:
+        bayerIMG_data = np.fromfile(bayerFile, dtype='uint8')
+    else:
+        bayerIMG_data = np.fromfile(bayerFile, dtype='uint16')
+    print("raw file size:", bayerIMG_data.size)
+
+    bayerIMG = bayerIMG_data.reshape(imgHeight, imgWidth, 1)
+
+    if bayerPattern == bayerFormat.BayerRGGB:
+        rgbIMG = cv2.cvtColor(bayerIMG, cv2.COLOR_BayerRG2RGB)
+    elif bayerPattern == bayerFormat.BayerGRBG:
+        rgbIMG = cv2.cvtColor(bayerIMG, cv2.COLOR_BayerGR2RGB)
+    elif bayerPattern == bayerFormat.BayerGBRG:
+        rgbIMG = cv2.cvtColor(bayerIMG, cv2.COLOR_BayerGB2RGB)
+    elif bayerPattern == bayerFormat.BayerBGGR:
+        rgbIMG = cv2.cvtColor(bayerIMG, cv2.COLOR_BayerBG2RGB)
+    else:
+        print("unsupport bayer format:", bayerPattern)
+
+    if bitDeepth == 8:
+        rgbIMG = np.uint8(rgbIMG)  # raw8
+    elif bitDeepth == 10:
+        rgbIMG = np.uint16(rgbIMG*64)  # raw10
+    elif bitDeepth == 12:
+        rgbIMG = np.uint16(rgbIMG*16)  # raw12
+    elif bitDeepth == 14:
+        rgbIMG = np.uint16(rgbIMG*4)  # raw14
+    elif bitDeepth == 16:
+        rgbIMG = np.uint16(rgbIMG)  # raw16
+    else:
+        print("unsupport bayer bitDeepth:", bitDeepth)
+
+    cv2.imwrite(bayerFile[:-4]+'.png', rgbIMG)
+
+    showColorImg(bayerFile, rgbIMG)
+    closeWindows()
+
+
+if "__main__" == __name__:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--rawfile", help="input raw file name", required=True, type=str)
+    parser.add_argument("--width", help="raw image width",
+                        required=True, type=int)
+    parser.add_argument("--height", help="raw image height",
+                        required=True, type=int)
+    parser.add_argument(
+        "--pattern", help="raw image bayer pattern: [1:RGGB, 2:GRBG, 3:GBRG, 4:BGGR]", required=True, type=int)
+    parser.add_argument(
+        "--depth", help="raw image depth [8, 10, 12, 14, 16]", required=True, type=int)
+
+    args = parser.parse_args()
+
+    rawFile = args.rawfile
+    img_width = args.width
+    img_height = args.height
+    bayerPattern = args.pattern
+    rawDepth = args.depth
+    convertBayer2RGB(rawFile, img_width, img_height, rawDepth, bayerPattern)
